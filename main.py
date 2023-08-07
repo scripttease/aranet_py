ae27fd75f10ac191438dc8396a360e7a360da8b3
Al Dee <amyalicedee@gmail.com>
2023-08-07 16:47:47 +0100

    Run after allowing in-app connection

--- main.py
+++ main.py
@@ -15,17 +15,17 @@ UUID_address = os.getenv("ARANET_UUID")
 
 async def main(address):
     async with BleakClient(address) as client:
-        print(client.is_connected)
-        # data = await client.read_gatt_char(CURRENT_READING)
-        # readings = struct.unpack('<HHHBBBHH', data)
-        # print(f'co2: {readings[0]}ppm')
-        # print(f'temperature: {readings[1]/20}C')
-        # print(f'pressure: {readings[2]/10}hPa')
-        # print(f'humidity: {readings[3]}%')
-        # print(f'battery: {readings[4]}%')
-        # print(f'status: {readings[5]}')
-        # print(f'interval: {readings[6]}s')
-        # print(f'age: {readings[7]}s')
+        # print(client.is_connected)
+        data = await client.read_gatt_char(CURRENT_READING)
+        readings = struct.unpack("<HHHBBBHH", data)
+        print(f"co2: {readings[0]}ppm")
+        print(f"temperature: {readings[1]/20}C")
+        print(f"pressure: {readings[2]/10}hPa")
+        print(f"humidity: {readings[3]}%")
+        print(f"battery: {readings[4]}%")
+        print(f"status: {readings[5]}")
+        print(f"interval: {readings[6]}s")
+        print(f"age: {readings[7]}s")
 
 
 asyncio.run(main(UUID_address))
